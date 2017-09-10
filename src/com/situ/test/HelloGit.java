5e5913dd4788f3d9a3cdc06b68b756e744ff8971
WenChuanmeng <1124775340@qq.com>
2017-09-10 11:32:11 +0800

    函数B

--- src/com/situ/test/HelloGit.java
+++ src/com/situ/test/HelloGit.java
@@ -10,8 +10,12 @@ public class HelloGit {
 		System.out.println("HelloGit.main()");
 		System.out.println("HelloGit.main()");
 	}
-	
+
 	public void A() {
 		System.out.println("AAAAAAAAAAAAAAAAAAAAAA");
+	}
+
+	public void B() {
+		System.out.println("BBBBBBBBBBBBBBBBBBBBBB");
 	}
 }
